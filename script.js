3ca5a3c56662b2f61f3dc68354766fad452822d5
juliry405 <fetzerw@yahoo.com>
2022-03-30 17:37:25 -0400

    Add script.js

--- script.js
+++ script.js
@@ -18,6 +18,12 @@ let playerSelection =  prompt("What's your selection?");
     alert("You Selected Paper! Paper Beats Rock!")
   }else if(playerSelection == "scissors" && computerSelection == "paper"){
     alert("You Selected Scissors! Scissors Beats Paper!")
+  }else if(playerSelection == "paper" && computerSelection == "paper"){
+    alert("It's a tie! You both selected paper!")
+  }else if(playerSelection == "scissors" && computerSelection == "scissors"){
+    alert("It's a tie! You both selected scissors!")
+  }else if(playerSelection == "rock" && computerSelection == "rock"){
+    alert("It's a tie! You both selected rock!")
   }
   
   if(computerSelection == "rock" && playerSelection == "scissors"){
@@ -26,5 +32,11 @@ let playerSelection =  prompt("What's your selection?");
     alert("The Computer Selected Scissors! Scissors Beats Paper!")
   }else if(computerSelection == "paper" && playerSelection == "rock"){
     alert("The Computer Selected Paper! Paper Beats Rock!")
+  }else if(computerSelection == "rock" && playerSelection == "rock"){
+    alert("It's a tie! You both selected rock!")
+  }else if(computerSelection == "scissors" && playerSelection == "scissors"){
+    alert("It's a tie! You both selected scissors!")
+  }else if(computerSelection == "paper" && playerSelection == "paper"){
+    alert("It's a tie! You both selected paper!")
   }
 }
